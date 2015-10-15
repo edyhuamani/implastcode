e3514530aefcc8662055075aa0c1da08bcb7b909
Edy . Huamani <edymartinh@gmail.com>
2015-10-15 00:17:07 -0300

    15-10-2015

---
+++ sismatp/src/main/java/pe/com/implast/model/beans/InformacionMateriaPrimaBean.java
@@ -0,0 +1,62 @@
+package pe.com.implast.model.beans;
+
+public class InformacionMateriaPrimaBean {
+	
+	private String pebd;
+	private String codigo;
+	private String tolvaInicial;
+	private String pisoInicial;
+	private String abastecido;
+	private String consumo;
+	private String tolvaFinal;
+	private String pisoFinal;
+	
+	public String getPebd() {
+		return pebd;
+	}
+	public void setPebd(String pebd) {
+		this.pebd = pebd;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getTolvaInicial() {
+		return tolvaInicial;
+	}
+	public void setTolvaInicial(String tolvaInicial) {
+		this.tolvaInicial = tolvaInicial;
+	}
+	public String getPisoInicial() {
+		return pisoInicial;
+	}
+	public void setPisoInicial(String pisoInicial) {
+		this.pisoInicial = pisoInicial;
+	}
+	public String getAbastecido() {
+		return abastecido;
+	}
+	public void setAbastecido(String abastecido) {
+		this.abastecido = abastecido;
+	}
+	public String getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(String consumo) {
+		this.consumo = consumo;
+	}
+	public String getTolvaFinal() {
+		return tolvaFinal;
+	}
+	public void setTolvaFinal(String tolvaFinal) {
+		this.tolvaFinal = tolvaFinal;
+	}
+	public String getPisoFinal() {
+		return pisoFinal;
+	}
+	public void setPisoFinal(String pisoFinal) {
+		this.pisoFinal = pisoFinal;
+	}
+}
